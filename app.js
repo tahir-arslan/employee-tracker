f695359d15d674dbb728390571bb9b86840649c2
ThisOneGuy <tahir.arslan@gmail.com>
2022-09-29 22:15:45 -0400

    completed employee crds

--- app.js
+++ app.js
@@ -46,9 +46,9 @@ Please select an option:`,
                 "Employee (Add New)",
                 "Employee (Update)",
                 "Employee (Delete)",
-                // "Roles (View All)",
-                // "Role (Add New)",
-                // "Role (Delete)",
+                "Roles (View All)",
+                "Role (Add New)",
+                "Role (Delete)",
                 // "Departments (View All)",
                 // "Department (Add New)",
                 // "Department (Delete)",
@@ -94,7 +94,7 @@ Please select an option:`,
     })
 };
 
-// view all employees
+// EMPLOYEE CRUD OPERATIONS
 function viewAllEmployees() {
     const sql = `SELECT employee.id AS ID, employee.first_name AS First_Name, employee.last_name AS Last_Name, role.title AS Role, department.name AS Department, role.salary AS Salary, employee.manager_id AS Manager 
     FROM employee
@@ -112,7 +112,6 @@ function viewAllEmployees() {
     });
 };
 
-// add employee (CREATE)
 function addEmployee () {
     inquirer.prompt([
         {
@@ -246,3 +245,92 @@ function deleteEmployee() {
         })
     })
 };
+
+// ROLE CRD OPERATIONS
+function viewAllRoles (){
+    let sqlViewRoles = `SELECT role.id AS Id, role.title AS Role, department.name AS department, role.salary AS Salary
+                FROM role
+                LEFT JOIN department
+                ON role.department_id = department.id `;
+    db.query(sqlViewRoles, (err, res) => {
+        if (err) {
+            console.log(err);
+            return;
+        } 
+        console.table(res);
+        startPrompt();
+    });
+}
+
+function addRole () {
+    inquirer.prompt([
+        {
+            type:'input',
+            name: 'role',
+            message: 'Please enter the role you wish to add :',
+        },
+        {
+            type: 'input',
+            name: 'salary',
+            message: "Please enter the salary for the role (ie: 125000) :",
+        }
+    ])
+    .then(roleChoice => {
+        const criteria = [roleChoice.role, roleChoice.salary];
+        const departmentSql ='SELECT department.id, department.name FROM department';
+        db.query(departmentSql, (err, result)=>{
+            const departments = result.map(({id, name}) =>({name: name, value:id}));
+            inquirer.prompt([
+                {
+                    type: 'list',
+                    name: 'department',
+                    message: "In which department does this role belong?",
+                    choices: departments
+                }
+            ])
+            .then (roleChoice => {
+                const role = roleChoice.department;
+                criteria.push(role);
+                const departmentSql = `INSERT INTO role (title, salary, department_id)
+                VALUES (?,?,?)`
+                db.query(departmentSql, criteria, (err, result) =>{
+                    if(err) {
+                        console.log(err.message);
+                        return;
+                    }
+                console.log("You have added a new role.")
+                startPrompt();
+                })
+            })
+        })
+    })  
+}
+
+function deleteRole() {
+    const roleSQL = `SELECT role.id, role.title FROM role`;
+    db.query(roleSQL, (err, result) =>{
+        const roles = result.map(({id, title}) => ({name: title, value:id}));
+        inquirer.prompt([
+        {
+            type: 'list',
+            name: 'role',
+            message: "Which Role would you like to delete?",
+            choices: roles
+        }
+        ])
+        .then (roleChoice =>{
+            const criteria = [roleChoice.role];
+            const deleteRole = `DELETE FROM role WHERE role.id = ?`;
+            db.query(deleteRole, criteria, (err, result) =>{
+                if(err) {
+                    console.log(err.message);
+                    return;
+                }
+                console.log("You have deleted that role");
+                startPrompt()
+            })
+        })
+    })
+}
+
+// DEPARTMENT CRD OPERATIONS
